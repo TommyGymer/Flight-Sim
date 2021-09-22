398c57f3ade1f64cc88be87592c80f024ce0457f
TommyGymer <tommygymer@gmail.com>
2021-09-23 00:33:09 +0100

    Removed old unbuilt
    Removed the old unbuilt directory which contained the unbuilt libs and source files

--- Unbuilt/external/assimp build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Obsidian/Programs/Flight Sim/Unbuilt/external/assimp - Copy/contrib/zlib
-# Build directory: D:/Obsidian/Programs/Flight Sim/Unbuilt/external/assimp build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
