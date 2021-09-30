a30e437bd22f823092de51fc1a51cb85d8c6721b
TommyGymer <tommygymer@gmail.com>
2021-09-30 13:36:35 +0100

    Object loading progress
    Need to move the loaded data to a holding structure before transfer over to OpenGL

--- Building/Libraries/Assimp.md
+++ Building/Libraries/Assimp.md
@@ -33,3 +33,67 @@ return true;
 This will load in the aiScene object from the specified file and check that the object is there
 
 [Using Assimp to import an object](https://ogldev.org/www/tutorial22/tutorial22.html)
+
+### Creating a loader class
+---
+To create a loader class a number of imports are required:
+
+```c++
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/mesh.h>
+#include <assimp/scene.h>
+#include <gl/glew.h>
+```
+
+A header file is also required
+
+```c++
+#include "Loader.hpp"
+```
+
+A load mesh function is defined which takes a filepath and loads the object file at that filepath
+
+```c++
+bool Loader::LoadMesh(const std::string& filename)
+{
+	bool Ret = false;
+	Assimp::Importer importer;
+
+	const aiScene* pScene = importer.ReadFile(filename.c_str(),
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType
+	);
+
+	if (pScene) {
+		Ret = InitFromScene(pScene, filename);
+	}
+	else {
+		printf("failed to load file");
+	}
+
+	return Ret;
+}
+```
+
+This also applies some postprocessing steps to clean up the loaded mesh and prepare for rendering
+
+An init from scene function is required to extract the data at the returned pointer from the loading
+
+```c++
+
+```
---
+++ RC-PC/RC-PC/ObjLoader.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/mesh.h>
+#include <assimp/scene.h>
+#include <gl/glew.h>
+
+#include "ObjLoader.hpp"
+
+bool ObjLoader::LoadMesh(const std::string& filename)
+{
+	bool Ret = false;
+	Assimp::Importer importer;
+
+	const aiScene* pScene = importer.ReadFile(filename.c_str(),
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType
+	);
+
+	if (pScene) {
+		Ret = InitFromScene(pScene, filename);
+	}
+	else {
+		printf("failed to load file");
+	}
+
+	return Ret;
+}
+
+bool ObjLoader::InitFromScene(const aiScene *pScene, const std::string& filename) {
+	m_Entries.resize(pScene->mNumMeshes);
+	m_Textures.resize(pScene->mNumMaterials);
+
+	//Init the meshes
+	for (unsigned int i = 0; i < m_Entries.size(); i++) {
+		const aiMesh* paiMesh = pScene->mMeshes[i];
+		InitMesh(i, paiMesh);
+	}
+
+	return InitMaterials(pScene, filename);
+}
+
+void ObjLoader::InitMesh(unsigned int Index, const aiMesh* paiMesh) {
+	m_Entries[Index].MaterialIndex = paiMesh->mMaterialIndex;
+
+	std::vector<int> Verticies;
+	std::vector<int> Indices;
+
+	const aiVector3D Origin(0.0f, 0.0f, 0.0f);
+
+	for (unsigned int i = 0; i < paiMesh->mNumVertices; i++) {
+		const aiVector3D* pPos = &(paiMesh->mVertices[i]);
+		const aiVector3D* pNormal = paiMesh->HasNormals() ? &(paiMesh->mNormals[i]) : &Origin;
+		const aiVector3D* pTexture = paiMesh->HasTextureCoords(0) ? &(paiMesh->mTextureCoords[0][i
+		]) : &Origin;
+
+		Vertex v(aiVector3D)
+	}
+}
