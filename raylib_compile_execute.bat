d242484458d6a55ca59f9025405506ffe3519541
TommyGymer <tommygymer@gmail.com>
2021-11-17 21:53:00 +0000

    Running RayLib examples from a portable file
    Now able to run the RayLib examples using only the files in the repo

--- raylib_compile_execute.bat
+++ raylib_compile_execute.bat
@@ -1,13 +1,13 @@
 ::@echo off
 :: .
-:: Compile your examples using:  raylib_compile_execute.bat core/core_basic_window.c
+:: Compile your examples using:  raylib_compile_execute.bat raylib/examples/core/core_basic_window.c
 :: .
 :: > Setup required Environment
 :: -------------------------------------
-set RAYLIB_INCLUDE_DIR="C:\Users\Tom\Documents\GitHub\Flight-Sim\raylib\src"
-set RAYLIB_LIB_DIR="C:\Users\Tom\Documents\GitHub\Flight-Sim\raylib\src"
-set RAYLIB_RES_FILE="C:\Users\Tom\Documents\GitHub\Flight-Sim\raylib\src\raylib.rc.data"
-set COMPILER_DIR="C:\Users\Tom\Documents\GitHub\Flight-Sim\tcc"
+set RAYLIB_INCLUDE_DIR=".\raylib\src"
+set RAYLIB_LIB_DIR=".\raylib\src"
+set RAYLIB_RES_FILE=".\raylib\src\raylib.rc.data"
+set COMPILER_DIR=".\tcc"
 set PATH=%PATH%;%COMPILER_DIR%
 :: Get full filename path for input file %1
 set FILENAME=%~f1
