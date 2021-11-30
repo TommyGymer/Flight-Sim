25bf630d5920e49877068abc556c13b07606b5ae
TommyGymer <tommygymer@gmail.com>
2021-11-30 15:41:42 +0000

    Added camera

--- RC-PC/src/main.cpp
+++ RC-PC/src/main.cpp
@@ -11,19 +11,29 @@ int main() {
     
     window.SetState(FLAG_WINDOW_RESIZABLE);
 
+    raylib::Camera camera(
+        raylib::Vector3(5.0f, 4.0f, 5.0f),
+        raylib::Vector3(0.0f, 2.0f, 0.0f),
+        raylib::Vector3(0.0f, 1.0f, 0.0f),
+        45.0f,
+        CAMERA_PERSPECTIVE);
+
+    camera.SetMode(CAMERA_ORBITAL);
+
     SetTargetFPS(60);
 
     // Main game loop
     while (!window.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
-
+        camera.Update();
         // Draw
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        {
+            ClearBackground(RAYWHITE);
 
-
-
+            DrawFPS(10, 10);
+        }
         EndDrawing();
     }
 
