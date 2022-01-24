dd853fa70dd3f1c4adee61dd3a995ca24018eea5
TommyGymer <tommygymer@gmail.com>
2022-01-24 15:29:55 +0000

    Rotating and tracking an object

--- RC-PC/src/main.cpp
+++ RC-PC/src/main.cpp
@@ -20,16 +20,16 @@ int main() {
 
     //raylib::Mesh mesh("obj/cube.obj");
     //raylib::Model plane("D:\\RC-PC\\obj\\box plane.obj");
-    raylib::Model plane("..\\obj\\materials.obj");
+    raylib::Model plane("..\\obj\\omega.obj");
 
     raylib::Camera3D camera(
-        raylib::Vector3(5.0f, 4.0f, 5.0f), //camera location
+        raylib::Vector3(50.0f, 20.0f, 50.0f), //camera location
         raylib::Vector3(0.0f, 0.0f, 0.0f), //camera look
         raylib::Vector3(0.0f, 1.0f, 0.0f),
         60.0f, //fov
         CAMERA_PERSPECTIVE);
 
-    camera.SetMode(CAMERA_ORBITAL);
+    //camera.SetMode(CAMERA_ORBITAL);
 
     raylib::Shader shader("vendor\\raylib-cpp\\vendor\\raylib\\examples\\shaders\\resources\\shaders\\glsl330\\base.vs", "vendor\\raylib-cpp\\vendor\\raylib\\examples\\shaders\\resources\\shaders\\glsl330\\base.fs");
     //raylib::Shader shader("D:\\RC-PC\\RC-PC\\vendor\\raylib-cpp\\vendor\\raylib\\examples\\shaders\\resources\\shaders\\glsl330\\base.vs", "D:\\RC-PC\\RC-PC\\vendor\\raylib-cpp\\vendor\\raylib\\examples\\shaders\\resources\\shaders\\glsl330\\grayscale.fs");
@@ -46,7 +46,7 @@ int main() {
     //considering using tiny object loader to load in the objects as this seems to be more powerful
     //https://github.com/tinyobjloader/tinyobjloader
 
-    SetTargetFPS(60);
+    SetTargetFPS(120);
 
     // Main game loop
     while (!window.ShouldClose()) // Detect window close button or ESC key
@@ -59,16 +59,18 @@ int main() {
             ClearBackground(RAYWHITE);
 
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
-            float obj_y = -(-50 - 0.5f * pow(duration.count() / 1000.0f, 2) * -9.81f) / 10.0;
-            float obj_x = sin(duration.count() / 350.0f);
-            float obj_z = sin(duration.count() / 1400.0f);
+            float obj_y = -(-50 - 0.5f * pow(duration.count() / 1000.0f, 2) * -9.81f);//fmod(-(-50 - 0.5f * pow(duration.count() / 1000.0f, 2) * -9.81f), 20.0);
+            float obj_x = 0;//sin(duration.count() / 350.0f);
+            float obj_z = 0;//sin(duration.count() / 1400.0f);
 
-            //camera.SetTarget(raylib::Vector3(obj_x, obj_y, obj_z));
+            camera.SetTarget(raylib::Vector3(obj_x, obj_y, obj_z));
 
             camera.BeginMode();
             {
                 DrawGrid(10, 1.0f);
-                plane.Draw(raylib::Vector3(obj_x, obj_y, obj_z), 1.0f);
+                
+                plane.Draw(raylib::Vector3(obj_x, obj_y, obj_z), raylib::Vector3(0, 1, 0), duration.count() * 1000, raylib::Vector3(1, 1, 1));
+                //camera.SetTarget(raylib::Vector3(obj_x, obj_y, obj_z))
             }
             camera.EndMode();
 
