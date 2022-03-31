3a9784e54bfd4ec51927b7e460e8003ea9ca5bee
TommyGymer <tommygymer@gmail.com>
2022-03-31 14:05:33 +0100

    Testing using catch2

--- RC-PC/src/fullMatrix.cpp
+++ RC-PC/src/fullMatrix.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <math.h>
 #include <raylib-cpp.hpp>
-#include "catch.hpp"
 
 enum class MatrixType {Vector, Coord, Matrix};
 
@@ -415,3 +414,14 @@ class fullMatrix {
 
         }*/
 };
+
+TEST_CASE("Vector arithmatic", "[fullMatrix]"){
+    fullMatrix a = fullMatrix(MatrixType::Vector, 1, 2, 3);
+    fullMatrix b = fullMatrix(MatrixType::Vector, 4, 5, 6);
+
+    SECTION("get"){
+        REQUIRE(a.x() == 1);
+        REQUIRE(a.y() == 2);
+        REQUIRE(a.x() == 3);
+    }
+}
