f038e2fd44f1d6f9c0e0ffd8fea0deeb17eb7802
TommyGymer <tommygymer@gmail.com>
2022-04-01 09:24:05 +0100

    Move RC-PC to build for testing

--- build/src/fullMatrix.cpp
+++ build/src/fullMatrix.cpp
@@ -414,14 +414,3 @@ class fullMatrix {
 
         }*/
 };
-
-TEST_CASE("Vector arithmatic", "[fullMatrix]"){
-    fullMatrix a = fullMatrix(MatrixType::Vector, 1, 2, 3);
-    fullMatrix b = fullMatrix(MatrixType::Vector, 4, 5, 6);
-
-    SECTION("get"){
-        REQUIRE(a.x() == 1);
-        REQUIRE(a.y() == 2);
-        REQUIRE(a.x() == 3);
-    }
-}
